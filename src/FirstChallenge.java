85ac840923f779912ea428c72894f32d942d649e
julio-silveira <julioqueiroz15@gmail.com>
2022-12-17 14:52:46 -0300

    feat: starting first challenge

---
+++ src/FirstChallenge.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class FirstChallenge {
+  private List<Integer> oddNumbers;
+  private List<Integer> evenNumbers;
+  
+  private void addToOdds(Integer number) { 
+    this.oddNumbers.add(number);
+  }
+
+  private void addToEven(Integer number) {
+    this.evenNumbers.add(number)
+  }
+
+  public void sortNumbers(int[] numbers ){
+
+  } 
+}
