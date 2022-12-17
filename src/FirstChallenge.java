92668c864f9620fd5673756b192ff79def9457d5
julio-silveira <julioqueiroz15@gmail.com>
2022-12-17 15:36:43 -0300

    feat: finished first challenge

--- src/FirstChallenge.java
+++ src/FirstChallenge.java
@@ -13,20 +13,34 @@ public class FirstChallenge {
   private void addToEven(Integer number) {
     this.evenNumbers.add(number);
   }
-
-  public void sortNumbers(int[] numbers ){
-    int sizeOfArray = numbers.length;
-    for(int i = 0; i < sizeOfArray; i += 1 ){
+  
+  private void oddOrEven(int[] numbers ){
+    for(int i = 0 ; i< numbers.length; i+= 1 ){
       if(numbers[i] % 2 == 0 ){
         this.addToEven(numbers[i]);
       } else {
         this.addToOdds(numbers[i]);
       }
- 
   }
-  Collections.sort(oddNumbers, Collections.reverseOrder());
-  Collections.sort(evenNumbers);
-  System.out.println(oddNumbers);
-  System.out.println(evenNumbers);
+  }
+
+  private void sortLists(){
+    Collections.sort(this.oddNumbers, Collections.reverseOrder());
+    Collections.sort(this.evenNumbers);
+  }
+
+  private void printLists(List<Integer> numbers){
+    for( int i = 0; i < numbers.size() ; i += 1){
+      System.out.println(numbers.get(i));
+    }
+  }
+
+  public void sortNumbers(int[] numbers ){
+    
+  this.oddOrEven(numbers);
+  this.sortLists();
+
+  this.printLists(evenNumbers);
+  this.printLists(oddNumbers);
 }
 }
