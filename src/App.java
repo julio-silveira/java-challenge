731d340abaaccd81ce5f4f65dcf4d15c57a9dae4
julio-silveira <julioqueiroz15@gmail.com>
2022-12-17 15:43:55 -0300

    chore: starting second challenge

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         FirstChallenge sort = new FirstChallenge();
-
         int [] numbers = { 10,4,32,34,543,3456,654,567,87,6789,98};
         sort.sortNumbers(numbers);
     }
