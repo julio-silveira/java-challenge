c58a66777e372dc80fad5a3ee4608fe439983a8b
julio-silveira <julioqueiroz15@gmail.com>
2022-12-17 17:22:35 -0300

    feat:  finished second challenge

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        FirstChallenge sort = new FirstChallenge();
-        int [] numbers = { 10,4,32,34,543,3456,654,567,87,6789,98};
-        sort.sortNumbers(numbers);
+        // FirstChallenge sort = new FirstChallenge();
+        // int [] numbers = { 10,4,32,34,543,3456,654,567,87,6789,98};
+        // sort.sortNumbers(numbers);
+
+        secondChallenge exchange = new secondChallenge();
+        double value = 576.74f;
+        exchange.toFiat(value);
     }
 }
--- src/secondChallenge.java
+++ src/secondChallenge.java
@@ -1,18 +1,32 @@
-public class secondChallenge {
-  private float currentValue;
-  private float[] bills = { 100.00f,50.00f, 20.00f, 5.00f, 2.00f };
-  private float[] coins = {1.00f, 0.50f, 0.25f, 0.10f, 0.05f, 0.01f };
+import java.util.Locale;
 
-  private void setCurrentValue(float currentValue) {
+public class secondChallenge {
+  private double currentValue;
+  private double[] bills = { 100.00f,50.00f, 20.00f, 10.00f ,5.00f, 2.00f };
+  private double[] coins = {1.00f, 0.50f, 0.25f, 0.10f, 0.05f, 0.01f };
+
+  private void setCurrentValue(double currentValue) {
     this.currentValue = currentValue;
   }
-  private float getCurrentValue() {
+  private double getCurrentValue() {
     return currentValue;
   }
 
-  
+  private void calcFiat(double moneyValue, String message) {
+    int moneyUnit = (int) Math.floor(this.getCurrentValue()/moneyValue);
+    double newValue = getCurrentValue() - (moneyUnit*moneyValue);
+    setCurrentValue(newValue);
+    System.out.println(String.format(Locale.US,"%d %s %.2f", moneyUnit, message, moneyValue));
+  }
 
-  public void toFiat(float value){
-    setCurrentValue(value);    
+  public void toFiat(double value){
+    this.setCurrentValue(value);
+    for(int i = 0; i< bills.length ; i += 1){
+      calcFiat(bills[i], " nota(s) de R$ ");
+    }
+    for(int i = 0; i< coins.length ; i += 1){
+      calcFiat(coins[i], " moedas(s) de R$ ");
+    }
+   
   }
 }
